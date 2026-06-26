35ffd2d5735b8d3522e912065bf3baeca19d1f05
arthi <arthi@gmail.com>
2026-06-26 13:27:45 +0530

    Removed Secret

--- Basic  AI Models/geminiapi.py
+++ Basic  AI Models/geminiapi.py
@@ -1,7 +1,7 @@
 # I want to become a data scientiest
 import google.generativeai as genai
 #Configuring API Key
-genai.configure(api_key="AQ.Ab8RN6IY1ZVR7ZLHr6dz1U-Yylw3Uj2Cdb6Y25sZc3aCSyMC3w")
+genai.configure(api_key="abc")
 #Loading Gemini Model
 print("AI CAREER GUIDANCE ASSISTANT")
 model=genai.GenerativeModel("gemini-3-flash-preview")
